419aa6b9b0e30b34e8faa4f49830b4dadd4a3bdc
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-05-02 15:28:27 +0300

    fix detail page

--- medicine/models.py
+++ medicine/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
@@ -23,7 +23,7 @@ class Medicine(models.Model):
     expiration_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    category = models.ManyToManyField(Category, related_name='category', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='category', null=True, blank=True)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
